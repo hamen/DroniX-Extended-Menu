1cce2d206682f0b66057f20cd653c9611972a90f
Ivan Morgillo <imorgillo@gmail.com>
2011-09-20 19:48:47 +0200

    Add WebServer start/stop capability to Preferences.java
    Some code refactoring and a new webserver start/stop method

--- src/org/dronix/android/dronixextendedmenu/Preferences.java
+++ src/org/dronix/android/dronixextendedmenu/Preferences.java
@@ -16,6 +16,7 @@ public class Preferences extends PreferenceActivity {
     boolean SSHCheckboxPreference;
     boolean WebServerCheckboxPreference;
     SSH ssh = new SSH(this);
+    WebServer wb = new WebServer(this);
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -24,41 +25,18 @@ public class Preferences extends PreferenceActivity {
 
             final CheckBoxPreference checkboxPref = (CheckBoxPreference) getPreferenceManager()
                 .findPreference("ssh_checkbox_preference");
-
             checkboxPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
                 public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    if (newValue.toString().equals("true") && !SSH.isRunning()) {
-                        try {
-                            ssh.start();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } catch (RootToolsException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        if (SSH.isRunning()) {
-                            Toast.makeText(getApplicationContext(),
-                                getText(R.string.sshStarted),
-                                Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                    else {
-                        try {
-                            ssh.stop();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } catch (RootToolsException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        if (!SSH.isRunning()) {
-                            Toast.makeText(getApplicationContext(),
-                                getText(R.string.sshStopped),
-                                Toast.LENGTH_SHORT).show();
-                        }
-                    }
+                    checkSSHstatus(newValue);
+                    return true;
+                }
+
+            });
+            final CheckBoxPreference webserverCeckboxPref = (CheckBoxPreference) getPreferenceManager()
+                .findPreference("webserver_checkbox_preference");
+            webserverCeckboxPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    checkWebServerStatus(newValue);
                     return true;
                 }
 
@@ -73,4 +51,65 @@ public class Preferences extends PreferenceActivity {
         WebServerCheckboxPreference = prefs.getBoolean("webserver_checkbox_preference", true);
     }
 
+    private void checkSSHstatus(Object newValue) {
+        if (newValue.toString().equals("true") && !SSH.isRunning()) {
+            try {
+                ssh.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (RootToolsException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (SSH.isRunning()) {
+                Toast.makeText(getApplicationContext(),
+                    getText(R.string.sshStarted),
+                    Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            try {
+                ssh.stop();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (RootToolsException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (!SSH.isRunning()) {
+                Toast.makeText(getApplicationContext(),
+                    getText(R.string.sshStopped),
+                    Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void checkWebServerStatus(Object newValue) {
+        if (newValue.toString().equals("true") && !WebServer.isRunning()) {
+            try {
+                wb.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (WebServer.isRunning()) {
+                Toast.makeText(getApplicationContext(),
+                    getText(R.string.webserverStarted),
+                    Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            try {
+                wb.stop();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (!WebServer.isRunning()) {
+                Toast.makeText(getApplicationContext(),
+                    getText(R.string.webserverStopped),
+                    Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
